2c9240eb083d310fe9599f1df9c65bbf86c790c0
Rishabh Kamboj <heyrishabh13@gmail.com>
2026-02-02 22:03:50 +0530

    controlled components - rendering filters

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ import NewExpense from "./components/NewExpense/NewExpense"
 function App() {
   const expenses = [
     { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
-    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
-    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
-    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
+    { id: "2", date: new Date(2024, 3, 25), title: "Book", price: 10 },
+    { id: "3", date: new Date(2025, 10, 11), title: "Pen", price: 1 },
+    { id: "4", date: new Date(2024, 1, 14), title: "Laptop", price: 200 },
   ];
   const [expensesData, setExpenses] = useState(expenses);
   const handleGetExpenseData = (expense)=>{
-    // console.log(expense);
     setExpenses((prevData)=>{
       return [...prevData, expense]
     });
